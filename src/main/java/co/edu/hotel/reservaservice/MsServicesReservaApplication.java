31f94955a0c4e2e3fdf8adb2841b6ef6d6d68b73
Camilo Garcia <juan092005ca@gmail.com>
2025-10-16 20:27:10 -0500

    First commit

---
+++ src/main/java/co/edu/hotel/reservaservice/MsServicesReservaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.hotel.reservaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServicesReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServicesReservaApplication.class, args);
+	}
+
+}
