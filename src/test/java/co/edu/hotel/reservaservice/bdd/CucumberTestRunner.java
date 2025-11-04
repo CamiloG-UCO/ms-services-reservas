be6bbcfd354380de57f55cb5a25ec9d1e9843d14
INachoRK <ignaciopatinocalle@gmail.com>
2025-11-04 09:47:51 -0500

    Pruebas BDD terminadas y probadas

---
+++ src/test/java/co/edu/hotel/reservaservice/bdd/CucumberTestRunner.java
@@ -0,0 +1,12 @@
+package co.edu.hotel.reservaservice.bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CucumberTestRunner {
+    
+    @Test
+    public void runCucumberTests() {
+    }
+}
---
+++ src/test/java/co/edu/hotel/reservaservice/bdd/TestConfig.java
@@ -0,0 +1,7 @@
+package co.edu.hotel.reservaservice.bdd;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class TestConfig {
+}
